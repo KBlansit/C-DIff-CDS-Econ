24a60ba5ce8a623b0b06d96d7b8d6e350e77f305
Kevin Blansit <kevin.blansit@gmail.com>
2017-06-13 02:39:21 -0700

    initial commit

---
+++ cds analysis.R
@@ -0,0 +1,44 @@
+
+# parameters####
+# set parameters to hash
+parameters<- list(
+  # cost parameters
+    
+  cBaseCostCDiffTx = 4000,
+  
+  cFalsePositiveInflatorMin = -1500,
+  cFalsePositiveInflatorMode = -100,
+  cFalsePositiveInflatorMax = -500,
+  
+  cFalseNegativeInflatorMin = -500,
+  cFalseNegativeInflatorMode = 250,
+  cFalseNegativeInflatorMax = 1000,
+  
+  cTrueNegative = 0,
+  
+  # test results
+  pPositiveResult = 0.4,
+  
+  pFalsePositiveNoCDS = 0.4,
+  pFalsePositiveYesCDS = 0.4,
+  
+  pFalseNegative = 0.5
+  
+)
+
+set.seed(2038)
+# main function
+
+
+
+negative <- function(parameters){
+  # case when false negative
+  if(runif(1, min=0, max=1) >= parameters$pFalseNegative) {
+    return(parameters$cBaseCostCDiffTx + parameters$cFalseNegativeInflator)
+  }
+  # case when true negative
+  else{
+    return(parameters$cTrueNegative)
+  }
+}
+
