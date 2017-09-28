84d432df27802ea6fd2d37fc5d48c64221b65c89
Kevin Blansit <kevin.blansit@gmail.com>
2017-09-27 22:00:14 -0700

    moretemp

--- run_simulation.py
+++ run_simulation.py
@@ -11,10 +11,10 @@ from multiprocessing import Pool
 from numpy.random import triangular
 
 # set global parameters
-numpy.random.seed(53845)
+np.random.seed(53845)
 random.seed(203876)
 
-NUMB_SIMS = 9999
+NUMB_SIMS = 99999
 
 # functions
 def get_new_dict(branch_item_dict):
@@ -29,41 +29,46 @@ def get_new_dict(branch_item_dict):
 
 def recusrive_process(dict_head):
 
-    # get branches
-    branches = dict_head['BRANCHES']
+    if 'BRANCHES' in dict_head:
+        # get branches
+        branches = dict_head['BRANCHES']
 
-    # get probs
-    prob_branches = [get_new_dict(x)['PROB'] for x in branches]
+        # get probs
+        prob_branches = [get_new_dict(x)['PROB'] for x in branches]
 
-    # assert probs add up to 1 and we only have 2 items
-    assert len(branches) == 2
-    assert sum(prob_branches) == 1
+        # assert probs add up to 1 and we only have 2 items
+        assert len(branches) == 2
+        assert sum(prob_branches) == 1
 
-    # determine which to use
-    if random.uniform(0, 1) >= prob_branches[0]:
-        selection = get_new_dict(branches[0])
-    else:
-        selection = get_new_dict(branches[1])
+        # determine which to use
+        if random.uniform(0, 1) >= prob_branches[0]:
+            selection = get_new_dict(branches[0])
+        else:
+            selection = get_new_dict(branches[1])
 
-    # return final cost if we can, otherwise return recusrive_process
-    if hasattr(selection, 'COST'):
-        return triangle(
-            selection['COST']['LOW'],
-            selection['COST']['MODE'],
-            selection['COST']['HIGH'],
+        recusrive_process(selection)
+
+        # return final cost if we can, otherwise return recusrive_process
+    elif 'COST' in dict_head:
+        return triangular(
+            dict_head['COST']['LOW'],
+            dict_head['COST']['MODE'],
+            dict_head['COST']['HIGH']
         )
     else:
-        recusrive_process(selection)
+        raise AssertionError(str(dict_head) + 'had a problem')
 
 # primary function
 def main():
     # read in data
     with open("parameters.yaml") as f:
-    model = yaml.load(f)
+        model = yaml.load(f)
 
     # make top level sims
     model_lst = [model['CDS']] * NUMB_SIMS + [model['NO_CDS']] * NUMB_SIMS
 
+    for i in model_lst:
+        recusrive_process(i)
     # make multiprocessing pool
     p = Pool()
 
@@ -76,3 +81,6 @@ def main():
 
     # pass to dataframe
     pd.DataFrame({'decision', 'cost'})
+if __name__ == '__main__':
+    main()
+
