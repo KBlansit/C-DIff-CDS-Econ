dfdff851fc6ee7e32dc8fde72fda19ffbfd53bb6
Kevin Blansit <kevin.blansit@gmail.com>
2017-09-27 21:22:45 -0700

    more commits

--- run_simulation.py
+++ run_simulation.py
@@ -4,10 +4,19 @@
 import yaml
 import random
 
+import numpy as np
 import pandas as pd
 
-# functions
+from multiprocessing import Pool
+from numpy.random import triangular
 
+# set global parameters
+numpy.random.seed(53845)
+random.seed(203876)
+
+NUMB_SIMS = 9999
+
+# functions
 def get_new_dict(branch_item_dict):
     # get vals
     vals = list(branch_item_dict.values())
@@ -38,15 +47,32 @@ def recusrive_process(dict_head):
 
     # return final cost if we can, otherwise return recusrive_process
     if hasattr(selection, 'COST'):
-        return selection['COST']
+        return triangle(
+            selection['COST']['LOW'],
+            selection['COST']['MODE'],
+            selection['COST']['HIGH'],
+        )
     else:
         recusrive_process(selection)
 
 # primary function
 def main():
-    # set random
-    random.seed(24258046)
-
     # read in data
     with open("parameters.yaml") as f:
     model = yaml.load(f)
+
+    # make top level sims
+    model_lst = [model['CDS']] * NUMB_SIMS + [model['NO_CDS']] * NUMB_SIMS
+
+    # make multiprocessing pool
+    p = Pool()
+
+    # map function
+    storage = p.map(recusrive_process, model_lst)
+
+    # clean up
+    p.close()
+    p.join()
+
+    # pass to dataframe
+    pd.DataFrame({'decision', 'cost'})
